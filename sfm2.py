a08fdc3e63ae37744d16c1e0fc4e747136a738f6
FlagArihant2000 <arihant.gaur@gmail.com>
2020-08-29 17:10:13 +0530

    added ba

--- sfm2.py
+++ sfm2.py
@@ -1,6 +1,12 @@
+# Structure from Motion
+# Author: Arihant Gaur
+# Organization: IvLabs, VNIT
+
+
 import cv2
 import numpy as np
 import os
+from scipy.optimize import least_squares
 
 
 def Triangulation(P1, P2, pts1, pts2, K):
@@ -13,37 +19,85 @@ def Triangulation(P1, P2, pts1, pts2, K):
 	
 	return points1, points2, cloud
 	
-def ReprojectionError(X, pts, Rt, K):
+def ReprojectionError(X, pts, Rt, K, homogenity):
 	total_error = 0
 	R = Rt[:3,:3]
 	t = Rt[:3,3]
 	
 	r, _ = cv2.Rodrigues(R)
-	X = cv2.convertPointsFromHomogeneous(X.T)
+	if homogenity == 1:
+		X = cv2.convertPointsFromHomogeneous(X.T)
+	
 	
 	p, _ = cv2.projectPoints(X, r, t, K, distCoeffs = None)
-	
 	p = p[:, 0, :]
-	#print(pts.shape, p.shape)
+	p = np.float32(p)
+	total_error = cv2.norm(p, pts.T, cv2.NORM_L2)
+	pts = pts.T
 	
-	error = cv2.norm(p, pts.T, cv2.NORM_L2)/len(p)
+	#for idx in range(len(p)):
+		#print(p[idx],pts[idx])
+	#	er = np.sqrt((p[idx][0] - pts[idx][0])**2 + (p[idx][1] - pts[idx][1])**2)
+	#	total_error = total_error + er
 	
-	tot_error = error ** 2
-	tot_error = tot_error / len(X)
+	
+	#print(p[0], pts.T[0])
+	
+	#tot_error = error ** 2
+	#tot_error = tot_error / len(X)
+	tot_error = total_error/len(p)
+	#print(p, pts.T)
 	
 	return tot_error, X
+	
+	
+def OptimReprojectionError(X_locs, p, r, t, K):
+	total_error = 0
+	p = p.T
+	num_pts = len(p)
+	R = X_locs[0:9].reshape((3,3))
+	t = X_locs[9:12]
+	K = X_locs[12:21].reshape((3,3))
+	X_locs = np.float32(X_locs[21:].reshape((num_pts, 1, 3)))
+	error = []
+	r, _ = cv2.Rodrigues(R)
+	p2d, _ = cv2.projectPoints(X_locs, r, t, K, distCoeffs = None)
+	p2d = p2d[:, 0, :]
+	#p, _ = cv2.projectPoints(X, r, t, K, distCoeffs = None)
+	for idx in range(num_pts):
+		img_pt = p[idx]
+		reprojected_pt = p2d[idx]
+		er = (img_pt - reprojected_pt)**2
+		error.append(er)
+
+	
+	return np.array(error).ravel()/num_pts
+	
+def BundleAdjustment(X, p, P, Rt, K):
+	num_points = len(p.T)
+	R = Rt[:3,:3]
+	t = Rt[:3,3]
+	opt_variables = np.hstack((R.ravel(), t.ravel()))
+	opt_variables = np.hstack((opt_variables,K.ravel()))
+	opt_variables = np.hstack((opt_variables, X.ravel()))
+	#print(X.shape, p.shape)
+	#print(P)
+	corrected_values = least_squares(OptimReprojectionError, opt_variables, args = (p, num_points, t, K))
+	#P = corrected_values.x[0:12].reshape(3,4)
+	#print(P)
+	corrected_values = corrected_values.x
+	R = corrected_values[0:9].reshape((3,3))
+	t = corrected_values[9:12].reshape((3,1))
+	K = corrected_values[12:21].reshape((3,3))
+	points_3d = corrected_values[21:].reshape((num_points, 1, 3))
+	#points_3d = corrected_values.x[12:].reshape((num_points, 3))
+	#print(R,t)
+	Rt = np.hstack((R,t))
+	P = np.matmul(K,Rt)
+	
+	return Rt, P, points_3d
 
 def to_ply(point_cloud, colors):
-	#colors = np.zeros_like(point_cloud)
-	#kp_new = np.array([kp[i][0] for i in range(len(kp))])
-	#kp_new = np.array(kp_new, dtype = np.int32)
-	#kp_new = kp
-	#colors = np.array([img2[l[1],l[0]] for l in kp_new])
-
-	#out_points = points[mask]
-	#out_colors = image[mask]
-	
-	#out_points = kp_new.reshape(-1,3)
 	out_points = point_cloud.reshape(-1,3)
 	out_colors = colors.reshape(-1,3)
 	verts = np.hstack([out_points, out_colors])
@@ -68,6 +122,7 @@ cv2.namedWindow('image1', cv2.WINDOW_NORMAL)
 cv2.namedWindow('image2', cv2.WINDOW_NORMAL)
 
 K = np.array([[2759.48, 0, 1520.69], [0, 2764.16, 1006.81], [0, 0, 1]])
+#K = np.array([[1520.400000, 0.000000, 302.320000], [0.000000, 1525.900000, 246.870000], [0.000000, 0.000000, 1.000000]])
 downscale = 2
 
 K[0,0] = K[0,0] / float(downscale)
@@ -86,6 +141,7 @@ P2 = np.empty((3,4))
 Xtot = np.zeros((1,3))
 colorstot = np.zeros((1,3))
 
+#img_dir = '/home/arihant/structure-from-motion/'
 img_dir = '/home/arihant/structure-from-motion/'
 
 img_list = sorted(os.listdir(img_dir))
@@ -94,9 +150,13 @@ for img in img_list:
 	if '.jpg' in img:
 		images = images + [img]
 i = 0		
+
+apply_ba = True
 while(i < len(images) - 1):
 	img0 = cv2.pyrDown(cv2.imread(img_dir + images[i]))
 	img1 = cv2.pyrDown(cv2.imread(img_dir + images[i + 1]))
+	#img0 = cv2.imread(img_dir + images[i])
+	#img1 = cv2.imread(img_dir + images[i + 1])
 	
 	img0gray = cv2.cvtColor(img0, cv2.COLOR_BGR2GRAY)
 	img1gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
@@ -131,22 +191,23 @@ while(i < len(images) - 1):
 	pts0, pts1, points_3d = Triangulation(P1, P2, pts0, pts1, K)
 	
 	#print(P1, P2)
-	error, points_3d = ReprojectionError(points_3d, pts1, R_t_1, K)
 	
-	Xtot = np.vstack((Xtot, points_3d[:, 0, :]))
-	
-	pts1_reg = np.array(pts1, dtype = np.int32)
-	colors = np.array([img1[l[1],l[0]] for l in pts1_reg.T])
-	colorstot = np.vstack((colorstot, colors))
-	
+	error, points_3d = ReprojectionError(points_3d, pts1, R_t_1, K, homogenity = 1)
 	print("Reprojection Error: ",error)
+	#print(points_3d[:, 0, :])
 	#print(pts1_reg.shape, points_3d.shape)
 	
 	#x = np.concatenate((x, points_3d[0]))
 	#y = np.concatenate((y, points_3d[1]))
 	#z = np.concatenate((z, points_3d[2]))
-	
-	
+	Xtot = np.vstack((Xtot, points_3d[:, 0, :]))
+	pts1_reg = np.array(pts1, dtype = np.int32)
+	colors = np.array([img1[l[1],l[0]] for l in pts1_reg.T])
+	colorstot = np.vstack((colorstot, colors))
+	if apply_ba == True:	
+		R_t_1, P2, points_3d = BundleAdjustment(points_3d, pts1, P2, R_t_1, K)
+		error, points_3d = ReprojectionError(points_3d, pts1, R_t_1, K, homogenity = 0)
+		print("Minimized Reprojection Error: ",error)
 	
 	cv2.imshow('image1', img0)
 	cv2.imshow('image2', img1)
